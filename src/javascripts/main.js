853ded024e1ebd698ee006bcb1d454cb5553aa27
Kelsey Creel <kelseycreel@gmail.com>
2019-10-02 21:09:04 -0500

    sleep

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,6 +2,7 @@ import '../styles/main.scss';
 import eat from './components/eat/eat';
 import play from './components/play/play';
 import fight from './components/fight/fight';
+import sleep from './components/sleep/sleep';
 
 const init = () => {
   eat.eatPrinter();
@@ -10,6 +11,8 @@ const init = () => {
   eat.hunger();
   fight.fightPrinter();
   fight.fightOrFlight();
+  sleep.sleepPrinter();
+  sleep.sleepy();
 };
 
 init();
