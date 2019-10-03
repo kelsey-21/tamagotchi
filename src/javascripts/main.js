ef176a272e999dd47bd54caf87547f2b839dc78a
Kelsey Creel <kelseycreel@gmail.com>
2019-10-02 20:33:43 -0500

    fight

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,12 +1,15 @@
 import '../styles/main.scss';
 import eat from './components/eat/eat';
 import play from './components/play/play';
+import fight from './components/fight/fight';
 
 const init = () => {
   eat.eatPrinter();
   play.playPrinter();
   play.playful();
   eat.hunger();
+  fight.fightPrinter();
+  fight.fightOrFlight();
 };
 
 init();
